2e08f76f3e67618f996d058655cccaacd1ccf122
h0l1da2 <whaleee@naver.com>
2023-08-04 21:40:41 +0900

    feat: WebService 추가
    Web 관련 서비스 추가
    Ok , Bad Response 메소드 추가

--- src/main/java/sori/jakku/kkunkkyu/memore/service/WebServiceImpl.java
+++ src/main/java/sori/jakku/kkunkkyu/memore/service/WebServiceImpl.java
@@ -1,2 +1,29 @@
-package sori.jakku.kkunkkyu.memore.service;public class WebServiceImpl {
+package sori.jakku.kkunkkyu.memore.service;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import sori.jakku.kkunkkyu.memore.service.inter.WebService;
+
+@Service
+@RequiredArgsConstructor
+public class WebServiceImpl implements WebService {
+
+    @Override
+    public ResponseEntity<String> okResponse(JsonObject jsonObject) {
+        Gson gson = new Gson();
+        String json = gson.toJson(jsonObject);
+        return ResponseEntity.ok()
+                .body(json);
+    }
+
+    @Override
+    public ResponseEntity<String> badResponse(JsonObject jsonObject) {
+        Gson gson = new Gson();
+        String json = gson.toJson(jsonObject);
+        return ResponseEntity.badRequest()
+                .body(json);
+    }
 }
--- src/main/java/sori/jakku/kkunkkyu/memore/service/inter/WebService.java
+++ src/main/java/sori/jakku/kkunkkyu/memore/service/inter/WebService.java
@@ -1,2 +1,9 @@
-package sori.jakku.kkunkkyu.memore.service.inter;public interface WebService {
+package sori.jakku.kkunkkyu.memore.service.inter;
+
+import com.google.gson.JsonObject;
+import org.springframework.http.ResponseEntity;
+
+public interface WebService {
+    ResponseEntity<String> okResponse(JsonObject jsonObject);
+    ResponseEntity<String> badResponse(JsonObject jsonObject);
 }
