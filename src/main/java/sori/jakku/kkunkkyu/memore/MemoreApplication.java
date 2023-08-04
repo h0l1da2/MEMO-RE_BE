8b2ba265cb77b2d921169408fc8561ee9480205b
h0l1da2 <whaleee@naver.com>
2023-08-04 17:09:04 +0900

    feat: Project Setting
    프로젝트 셋팅

---
+++ src/main/java/sori/jakku/kkunkkyu/memore/MemoreApplication.java
@@ -0,0 +1,13 @@
+package sori.jakku.kkunkkyu.memore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sori/jakku/kkunkkyu/memore/MemoreApplicationTests.java
@@ -0,0 +1,13 @@
+package sori.jakku.kkunkkyu.memore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
