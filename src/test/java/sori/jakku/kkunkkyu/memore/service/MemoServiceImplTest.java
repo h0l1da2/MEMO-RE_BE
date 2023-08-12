b93fc22140788cd5d3537d1fec8d4c78003fc8c8
h0l1da2 <whaleee@naver.com>
2023-08-12 13:01:22 +0900

    test: 메모 CRUD TEST
    메모쓰기, 수정, 삭제 테스트
    (서비스)

--- src/test/java/sori/jakku/kkunkkyu/memore/service/MemoServiceImplTest.java
+++ src/test/java/sori/jakku/kkunkkyu/memore/service/MemoServiceImplTest.java
@@ -1,5 +1,6 @@
 package sori.jakku.kkunkkyu.memore.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,11 @@ import sori.jakku.kkunkkyu.memore.domain.Memo;
 import sori.jakku.kkunkkyu.memore.domain.Tag;
 import sori.jakku.kkunkkyu.memore.domain.TagMemo;
 import sori.jakku.kkunkkyu.memore.domain.User;
+import sori.jakku.kkunkkyu.memore.domain.dto.ConTagUpdateDto;
 import sori.jakku.kkunkkyu.memore.domain.dto.MemoWriteDto;
 import sori.jakku.kkunkkyu.memore.exception.DuplicateMemoException;
+import sori.jakku.kkunkkyu.memore.exception.MemoNotFoundException;
+import sori.jakku.kkunkkyu.memore.exception.UserNotFoundException;
 import sori.jakku.kkunkkyu.memore.repository.CustomTagMemoRepository;
 import sori.jakku.kkunkkyu.memore.repository.MemoRepository;
 import sori.jakku.kkunkkyu.memore.repository.TagRepository;
@@ -19,7 +23,9 @@ import sori.jakku.kkunkkyu.memore.repository.UserRepository;
 import sori.jakku.kkunkkyu.memore.service.inter.MemoService;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -170,11 +176,114 @@ class MemoServiceImplTest {
 
     }
 
-//    @Test
-//    void changeContentTag() {
-//    }
-//
-//    @Test
-//    void removeMemo() {
-//    }
+    @Test
+    @DisplayName("메모 수정 성공 : 모든 값")
+    void 메모수정_성공() throws DuplicateMemoException, UserNotFoundException, MemoNotFoundException {
+        User user = new User();
+        User newUser = userRepository.save(user);
+        List<String> list = new ArrayList<>();
+        list.add("tag1");
+        MemoWriteDto memoWriteDto = new MemoWriteDto("keyword", "content", list);
+        memoService.write(newUser.getId(), memoWriteDto);
+
+        ConTagUpdateDto conTagUpdateDto = new ConTagUpdateDto();
+        conTagUpdateDto.setOriginKey("keyword");
+        conTagUpdateDto.setNewKey("newKey");
+        conTagUpdateDto.setContent("newCont");
+        Map<String, Boolean> map = new HashMap();
+        map.put("tag2", true);
+        map.put("tag1", false);
+        conTagUpdateDto.setTag(map);
+
+        memoService.changeContentTag(newUser.getId(), conTagUpdateDto);
+
+        Memo memo = memoRepository.findByKeyword("newKey").orElse(null);
+        Tag tag1 = tagRepository.findByName("tag1").orElse(null);
+        Tag tag2 = tagRepository.findByName("tag2").orElse(null);
+        List<TagMemo> allTagMemoByMemo = tagMemoRepository.findAllTagMemoByMemo(memo);
+
+        assertThat(memo).isNotNull();
+        assertThat(memo.getKeyword()).isEqualTo("newKey");
+        assertThat(memo.getContent()).isEqualTo("newCont");
+        assertThat(tag1).isNotNull();
+        assertThat(tag2).isNotNull();
+        assertThat(allTagMemoByMemo.get(0).getTag().getId()).isEqualTo(tag2.getId());
+        assertThat(allTagMemoByMemo.size()).isEqualTo(1);
+
+    }
+    @Test
+    @DisplayName("메모 수정 성공 : 태그 없음")
+    void 메모수정_태그없음_성공() throws DuplicateMemoException, UserNotFoundException, MemoNotFoundException {
+        User user = new User();
+        User newUser = userRepository.save(user);
+        List<String> list = new ArrayList<>();
+        list.add("tag1");
+        MemoWriteDto memoWriteDto = new MemoWriteDto("keyword", "content", list);
+        memoService.write(newUser.getId(), memoWriteDto);
+
+        ConTagUpdateDto conTagUpdateDto = new ConTagUpdateDto();
+        conTagUpdateDto.setOriginKey("keyword");
+        conTagUpdateDto.setNewKey("newKey");
+        conTagUpdateDto.setContent("newCont");
+        Map<String, Boolean> map = new HashMap();
+        map.put("tag1", false);
+        conTagUpdateDto.setTag(map);
+
+        memoService.changeContentTag(newUser.getId(), conTagUpdateDto);
+
+        Memo memo = memoRepository.findByKeyword("newKey").orElse(null);
+        Tag tag1 = tagRepository.findByName("tag1").orElse(null);
+        List<TagMemo> allTagMemoByMemo = tagMemoRepository.findAllTagMemoByMemo(memo);
+
+        assertThat(memo).isNotNull();
+        assertThat(memo.getKeyword()).isEqualTo("newKey");
+        assertThat(memo.getContent()).isEqualTo("newCont");
+        assertThat(tag1).isNotNull();
+        assertThat(allTagMemoByMemo.size()).isEqualTo(0);
+
+
+    }
+    @Test
+    @DisplayName("메모 수정 실패 : 키워드 실패")
+    void 메모수정_키워드_실패() throws DuplicateMemoException {
+        User user = new User();
+        User newUser = userRepository.save(user);
+        List<String> list = new ArrayList<>();
+        list.add("tag1");
+        MemoWriteDto memoWriteDto = new MemoWriteDto("keyword", "content", list);
+        memoService.write(newUser.getId(), memoWriteDto);
+
+        ConTagUpdateDto conTagUpdateDto = new ConTagUpdateDto();
+        conTagUpdateDto.setOriginKey("no_keyword");
+        conTagUpdateDto.setNewKey("newKey");
+        conTagUpdateDto.setContent("newCont");
+        Map<String, Boolean> map = new HashMap();
+        map.put("tag2", true);
+        map.put("tag1", false);
+        conTagUpdateDto.setTag(map);
+
+        Assertions.assertThrows(MemoNotFoundException.class, () ->
+                memoService.changeContentTag(newUser.getId(), conTagUpdateDto));
+    }
+
+    @Test
+    @DisplayName("메모 삭제 성공")
+    void removeMemo() throws DuplicateMemoException, UserNotFoundException, MemoNotFoundException {
+        User user = new User();
+        User newUser = userRepository.save(user);
+        List<String> list = new ArrayList<>();
+        list.add("tag1");
+        MemoWriteDto memoWriteDto = new MemoWriteDto("keyword", "content", list);
+        memoService.write(newUser.getId(), memoWriteDto);
+
+        memoService.removeMemo(user.getId(), "keyword");
+
+        Memo memo = memoRepository.findByKeyword("keyword").orElse(null);
+        Tag tag = tagRepository.findByName("tag1").orElse(null);
+        List<TagMemo> allTagMemoByMemo = tagMemoRepository.findAllTagMemo();
+
+        assertThat(memo).isNull();
+        assertThat(tag).isNotNull();
+        assertThat(allTagMemoByMemo.size()).isEqualTo(0);
+    }
 }
